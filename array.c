4b322aeba607bb0cff7498f9b6b2d123f520bc95
wxang <41978782+wxang@users.noreply.github.com>
2018-08-03 12:00:36 +0800

    Create Data Type

--- array.c
+++ array.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+typedef struct Array{
+	int *p_base;
+	int length;
+	int volume;
+}ARRAY,*P_ARRAY;
+
+bool array_create(P_ARRAY, int);
+bool array_destroy(P_ARRAY);
+bool array_traverse(P_ARRAY);
+bool node_insert(P_ARRAY, int, int);
+bool node_delete(P_ARRAY, int);
+bool node_search(P_ARRAY, int);
+
+int main(void){
+	ARRAY my_array;
+	return 0;
+}
